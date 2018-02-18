fc65efe140d7451a4bcf4aa3adc3da88804e9454
allenhe <hymeldon@gmail.com>
2018-02-17 23:08:02 -0500

    Add remote server core code.
    Including:
    Listening module - connection with other proxy servers
    Forwarding module - forwarding requests from proxy servers

---
+++ remote/remote.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	//	"bytes"
+	"fmt"
+	//	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+type remoteServer struct {
+	ip   string
+	port string
+}
+
+func NewRemoteServer(ip string, port string) remoteServer {
+	server := remoteServer{
+		ip:   ip,
+		port: port,
+	}
+
+	return server
+}
+
+func proxyHandler(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("-----New connection from " + conn.RemoteAddr().String())
+	conn_reader := bufio.NewReader(conn)
+	req, err := http.ReadRequest(conn_reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.URL, err = url.Parse(req.RequestURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.RequestURI = ""
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp_bytes, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn_writer := bufio.NewWriter(conn)
+	conn_writer.Write(resp_bytes)
+	conn_writer.Flush()
+}
+
+func (server remoteServer) Run() {
+	ln, err := net.Listen("tcp", server.ip+":"+server.port)
+	fmt.Println(ln)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		//		defer conn.Close()
+
+		go proxyHandler(conn)
+	}
+}
+
+func main() {
+	server := NewRemoteServer("127.0.0.1", "18999")
+	server.Run()
+}
