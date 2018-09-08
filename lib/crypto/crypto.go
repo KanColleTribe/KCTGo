2ce255ae70988a9152a1a22cd2f1b0c8d2f286e7
wuyuMk7 <hymeldon@gmail.com>
2018-09-08 15:05:29 -0400

    feat(crypto): Add HMAC, Signature, Key file read methods and tests
    fix(crypto): Remove rand.Reader from RSA methods

--- lib/crypto/crypto.go
+++ lib/crypto/crypto.go
@@ -1,18 +1,30 @@
 package crypto
 
 import (
+	"crypto"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/des"
+	"crypto/hmac"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"io"
+	"io/ioutil"
 )
 
 type Crypto struct {
 	mode  string
 	nonce []byte
 	label []byte
+}
+
+var pssOption = rsa.PSSOptions{
+	SaltLength: rsa.PSSSaltLengthAuto,
+	Hash:       crypto.SHA256,
 }
 
 func (c Crypto) Encrypt(plaintext []byte, key []byte) ([]byte, error) {
@@ -79,14 +91,91 @@ func (c Crypto) Decrypt(ciphertext []byte, key []byte) ([]byte, error) {
 	return plaintext, err
 }
 
-func (c Crypto) RSAEncrypt(plaintext []byte, pk *rsa.PublicKey) ([]byte, error) {
-	ciphertext, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, pk, plaintext, c.label)
+// RSA
+
+func (c Crypto) RSAEncrypt(plaintext []byte, pk *rsa.PublicKey, random io.Reader) ([]byte, error) {
+	ciphertext, err := rsa.EncryptOAEP(sha256.New(), random, pk, plaintext, c.label)
 
 	return ciphertext, err
 }
 
-func (c Crypto) RSADecrypt(ciphertext []byte, sk *rsa.PrivateKey) ([]byte, error) {
-	plaintext, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, sk, ciphertext, c.label)
+func (c Crypto) RSADecrypt(ciphertext []byte, sk *rsa.PrivateKey, random io.Reader) ([]byte, error) {
+	plaintext, err := rsa.DecryptOAEP(sha256.New(), random, sk, ciphertext, c.label)
 
 	return plaintext, err
+}
+
+// For HMAC
+
+func GetHMAC(content []byte, key []byte) []byte {
+	h := hmac.New(sha256.New, key)
+	h.Write(content)
+	return h.Sum(nil)
+}
+
+func CheckHMAC(content1 []byte, content2 []byte) bool {
+	return hmac.Equal(content1, content2)
+}
+
+// Signature
+
+func Sign(content []byte, sk *rsa.PrivateKey) ([]byte, error) {
+	hashed := sha256.Sum256(content)
+	signature, err := rsa.SignPSS(rand.Reader, sk, crypto.SHA256, hashed[:], &pssOption)
+
+	return signature, err
+}
+
+func VerifySignature(content []byte, signature []byte, pk *rsa.PublicKey) error {
+	hashed := sha256.Sum256(content)
+	return rsa.VerifyPSS(pk, crypto.SHA256, hashed[:], signature, &pssOption)
+}
+
+// Load && Save key files
+
+func LoadSK(filename string, passphrase []byte) (rsa.PrivateKey, error) {
+	var sk = &rsa.PrivateKey{}
+	var err error
+
+	key, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return *sk, err
+	}
+
+	block, _ := pem.Decode(key)
+	if block == nil {
+		return *sk, errors.New("Empty block. Please check your key file!")
+	}
+	if len(passphrase) > 0 {
+		decrypted, err := x509.DecryptPEMBlock(block, passphrase)
+		if err != nil {
+			return *sk, err
+		}
+		sk, err = x509.ParsePKCS1PrivateKey(decrypted)
+	} else {
+		sk, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+	}
+
+	if err != nil {
+		return rsa.PrivateKey{}, err
+	} else {
+		return *sk, err
+	}
+}
+
+func LoadPK(filename string) (interface{}, error) {
+	var err error
+
+	key, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(key)
+	pk, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	} else {
+		return pk, err
+	}
 }
